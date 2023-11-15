cbadebeae0aa1dafbb970da90c8881a11a2e9149
Huilde Viana Junior <hvianajunior@apple.com>
2023-11-14 23:45:09 -0300

    feat: implement new screens

---
+++ AndroidStudioProjects/CalculadoraIMD2/app/src/main/java/com/example/calculadoraimd/cadastrar.java
@@ -0,0 +1,32 @@
+package com.example.calculadoraimd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class cadastrar extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar);
+
+//        btnCadastrar = findViewById(R.id.Cadastrar);
+//        btnCadastrar.setOnClickListener(v -> {
+//            Intent intent = new Intent(menu.this, cadastrar.class);
+//
+//            // Inicie a nova atividade
+//            startActivity(intent);
+//        });
+
+
+    }
+
+}
--- AndroidStudioProjects/CalculadoraIMD2/app/src/main/java/com/example/calculadoraimd/menu.java
+++ AndroidStudioProjects/CalculadoraIMD2/app/src/main/java/com/example/calculadoraimd/menu.java
@@ -1,18 +1,13 @@
 package com.example.calculadoraimd;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
+import android.widget.TextView;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.calculadoraimd.databinding.ActivityMenuBinding;
 
@@ -20,14 +15,43 @@ public class menu extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMenuBinding binding;
-
+    private TextView btnCadastrar, btnListar, btnAlterar, btnDeletar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
 
-        binding = ActivityMenuBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        btnCadastrar = findViewById(R.id.cadastrar);
+        btnAlterar = findViewById(R.id.alterar);
+        btnDeletar = findViewById(R.id.voltar);
+        btnListar = findViewById(R.id.listar);
+        btnCadastrar.setOnClickListener(v -> {
+            Intent intent = new Intent(menu.this, cadastrar.class);
 
+            // Inicie a nova atividade
+            startActivity(intent);
+        });
+
+        btnAlterar.setOnClickListener(v -> {
+            Intent intent = new Intent(menu.this, alterar.class);
+
+            // Inicie a nova atividade
+            startActivity(intent);
+        });
+
+        btnDeletar.setOnClickListener(v -> {
+            Intent intent = new Intent(menu.this, deletar.class);
+
+            // Inicie a nova atividade
+            startActivity(intent);
+        });
+
+        btnListar.setOnClickListener(v -> {
+            Intent intent = new Intent(menu.this, listar.class);
+
+            // Inicie a nova atividade
+            startActivity(intent);
+        });
 
 
     }
